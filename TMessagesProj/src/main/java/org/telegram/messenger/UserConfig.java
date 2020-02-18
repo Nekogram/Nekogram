cc4402d428abdda64e5fb592e8137516f38cd8e3
NekoInverter <42698724+NekoInverter@users.noreply.github.com>
2020-02-18 17:55:29 +0800

    Revert "max account to 32"
    This reverts commit a83dd3b1

--- TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
@@ -22,7 +22,7 @@ import java.io.File;
 public class UserConfig extends BaseController {
 
     public static int selectedAccount;
-    public final static int MAX_ACCOUNT_COUNT = 32;
+    public final static int MAX_ACCOUNT_COUNT = 8;
 
     private final Object sync = new Object();
     private boolean configLoaded;
