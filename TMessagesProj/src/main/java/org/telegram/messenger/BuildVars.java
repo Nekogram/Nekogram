30810a6a55292a6c3b194e638770ceb6e737b41e
NekoInverter <42698724+NekoInverter@users.noreply.github.com>
2020-04-06 10:40:33 +0800

    Bump version

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -19,15 +19,15 @@ public class BuildVars {
     public static boolean USE_CLOUD_STRINGS = true;
     public static boolean CHECK_UPDATES = true;
     public static boolean TON_WALLET_STANDALONE = false;
-    public static int BUILD_VERSION = 1911;
-    public static String BUILD_VERSION_STRING = "6.0.0";
+    public static int BUILD_VERSION = 1912;
+    public static String BUILD_VERSION_STRING = "6.0.1.1";
     public static int APP_ID = 336779;
     public static String APP_HASH = "b91eefacc86747c068c8d8a16b41500d";
     public static String APPCENTER_HASH = "a5b5c4f5-51da-dedc-9918-d9766a22ca7c";
     public static String APPCENTER_HASH_DEBUG = "f9726602-67c9-48d2-b5d0-4761f1c1a8f3";
     //
     public static String SMS_HASH = DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT";
-    public static String PLAYSTORE_APP_URL = "\"https://play.google.com/store/apps/details?id=tw.nekomimi.nekogram";
+    public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=tw.nekomimi.nekogram";
 
     static {
         if (ApplicationLoader.applicationContext != null) {
