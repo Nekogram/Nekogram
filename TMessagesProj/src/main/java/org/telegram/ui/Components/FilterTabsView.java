cbec0cc444aec9fb1bbf7bea5846b64dedeea4de
NekoInverter <42698724+NekoInverter@users.noreply.github.com>
2020-04-17 13:25:06 +0800

    Fix allow hide "All Chats"

--- TMessagesProj/src/main/java/org/telegram/ui/Components/FilterTabsView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/FilterTabsView.java
@@ -968,7 +968,7 @@ public class FilterTabsView extends FrameLayout {
                 requestLayout();
                 adapter.notifyDataSetChanged();
                 allTabsWidth = 0;
-                tabs.get(0).setTitle(LocaleController.getString("FilterAllChats", R.string.FilterAllChats));
+                if (!NekoConfig.hideAllTab) tabs.get(0).setTitle(LocaleController.getString("FilterAllChats", R.string.FilterAllChats));
                 for (int b = 0; b < N; b++) {
                     allTabsWidth += tabs.get(b).getWidth(true) + AndroidUtilities.dp(32);
                 }
@@ -997,7 +997,7 @@ public class FilterTabsView extends FrameLayout {
             requestLayout();
             adapter.notifyDataSetChanged();
             allTabsWidth = 0;
-            tabs.get(0).setTitle(LocaleController.getString("FilterAllChats", R.string.FilterAllChats));
+            if (!NekoConfig.hideAllTab) tabs.get(0).setTitle(LocaleController.getString("FilterAllChats", R.string.FilterAllChats));
             for (int b = 0, N = tabs.size(); b < N; b++) {
                 allTabsWidth += tabs.get(b).getWidth(true) + AndroidUtilities.dp(32);
             }
