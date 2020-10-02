5026031a4b38577f2a99ff610fb35969acf1c1b3
NekoInverter <42698724+NekoInverter@users.noreply.github.com>
2020-10-02 18:27:52 +0800

    Fix repeating albums

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -16188,7 +16188,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                             }
                             if (NekoConfig.showTranslate) {
                                 MessageObject messageObject = null;
-                                if (selectedObjectGroup != null && selectedObjectGroup.messages.size() != 0) {
+                                if (selectedObjectGroup != null) {
                                     if (!TextUtils.isEmpty(selectedObjectGroup.messages.get(0).messageOwner.message)) {
                                         messageObject = selectedObjectGroup.messages.get(0);
                                     }
@@ -17247,7 +17247,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 break;
             } case 88: {
                 MessageObject messageObject = null;
-                if (selectedObjectGroup != null && selectedObjectGroup.messages.size() != 0) {
+                if (selectedObjectGroup != null) {
                     if (!TextUtils.isEmpty(selectedObjectGroup.messages.get(0).messageOwner.message)) {
                         messageObject = selectedObjectGroup.messages.get(0);
                     }
@@ -17396,7 +17396,11 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     return;
                 }
                 ArrayList<MessageObject> messages =  new ArrayList<>();
-                messages.add(selectedObject);
+                if (selectedObjectGroup != null) {
+                    messages.addAll(selectedObjectGroup.messages);
+                } else {
+                    messages.add(selectedObject);
+                }
                 forwardMessages(messages, false, true, 0);
                 break;
             } case 95: {
