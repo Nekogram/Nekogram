d08d40c0fba45a93394e80e6adfbe673a3f93eca
NekoInverter <chsqwyx@gmail.com>
2021-12-07 22:09:09 +0800

    Check default send as in delete history alert

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/MessageHelper.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/MessageHelper.java
@@ -204,8 +204,9 @@ public class MessageHelper extends BaseController {
         frameLayout.addView(messageTextView, LayoutHelper.createFrame(LayoutHelper.WRAP_CONTENT, LayoutHelper.WRAP_CONTENT, (LocaleController.isRTL ? Gravity.RIGHT : Gravity.LEFT) | Gravity.TOP, 24, 57, 24, 9));
 
         if (cell != null) {
+            TLRPC.ChatFull chatFull = getMessagesController().getChatFull(chat.id);
             cell.setBackground(Theme.getSelectorDrawable(false));
-            cell.setText(LocaleController.getString("DeleteAllFromSelfAdmin", R.string.DeleteAllFromSelfAdmin), "", !ChatObject.shouldSendAnonymously(chat)/* send_as?*/, false);
+            cell.setText(LocaleController.getString("DeleteAllFromSelfAdmin", R.string.DeleteAllFromSelfAdmin), "", !ChatObject.shouldSendAnonymously(chat) && chatFull.default_send_as.user_id == getUserConfig().clientUserId, false);
             cell.setPadding(LocaleController.isRTL ? AndroidUtilities.dp(16) : AndroidUtilities.dp(8), 0, LocaleController.isRTL ? AndroidUtilities.dp(8) : AndroidUtilities.dp(16), 0);
             frameLayout.addView(cell, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, 48, Gravity.BOTTOM | Gravity.LEFT, 0, 0, 0, 0));
             cell.setOnClickListener(v -> {
