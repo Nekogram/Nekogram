d52188a37c6e5c510c89ea6e744e6062989232f5
NekoInverter <chsqwyx@gmail.com>
2021-12-16 12:31:16 +0800

    Fix clickable area for ManageChatUserCell

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/ManageChatUserCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/ManageChatUserCell.java
@@ -138,7 +138,7 @@ public class ManageChatUserCell extends FrameLayout {
         currentObject = object;
         if (optionsButton != null) {
             boolean visible = delegate.onOptionsButtonCheck(ManageChatUserCell.this, false);
-            optionsButton.setVisibility(visible ? VISIBLE : INVISIBLE);
+            optionsButton.setVisibility(visible ? VISIBLE : GONE);
             nameTextView.setLayoutParams(LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, 20, (LocaleController.isRTL ? Gravity.RIGHT : Gravity.LEFT) | Gravity.TOP, LocaleController.isRTL ? (visible ? 46 : 28) : (68 + namePadding), status == null || status.length() > 0 ? 11.5f : 20.5f, LocaleController.isRTL ? (68 + namePadding) : (visible ? 46 : 28), 0));
             statusTextView.setLayoutParams(LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, 20, (LocaleController.isRTL ? Gravity.RIGHT : Gravity.LEFT) | Gravity.TOP, LocaleController.isRTL ? (visible ? 46 : 28) : (68 + namePadding), 34.5f, LocaleController.isRTL ? (68 + namePadding) : (visible ? 46 : 28), 0));
         } else if (customImageView != null) {
