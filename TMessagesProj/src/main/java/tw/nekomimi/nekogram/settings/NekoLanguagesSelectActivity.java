456d2500f81925ef1ae3018692ab6cc327150006
tehcneko <7741672-NekoInverter@users.noreply.gitlab.com>
2022-01-31 19:34:02 +0800

    Update language detection list

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoLanguagesSelectActivity.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoLanguagesSelectActivity.java
@@ -46,16 +46,17 @@ public class NekoLanguagesSelectActivity extends BaseFragment {
     public static final int TYPE_TARGET = 1;
 
     private static final List<String> RESTRICTED_LIST = Arrays.asList(
-            "af", "am", "ar", "ar", "az", "be", "bg", "bn", "bs", "ca", "ceb",
-            "co", "cs", "cy", "da", "de", "el", "en", "eo", "es", "et", "eu",
-            "fa", "fi", "fil", "fr", "fy", "ga", "gd", "gl", "gu", "ha", "haw",
-            "he", "hi", "hmn", "hr", "ht", "hu", "hy", "id", "ig", "is", "it",
-            "ja", "jv", "ka", "kk", "km", "kn", "ko", "ku", "ky", "la", "lb",
-            "lo", "lt", "lv", "mg", "mi", "mk", "ml", "mn", "mr", "ms", "mt",
-            "my", "ne", "nl", "no", "ny", "pa", "pl", "ps", "pt", "ro", "ru",
-            "sd", "si", "sk", "sl", "sm", "sn", "so", "sq", "sr", "st", "su",
-            "sv", "sw", "ta", "te", "tg", "th", "tr", "uk", "ur", "uz", "vi",
-            "xh", "yi", "yo", "zh", "zu");
+            "af", "am", "ar", "az", "be", "bg", "bn", "bs", "ca", "ceb",
+            "co", "cs", "cy", "da", "de", "el", "en", "eo", "es", "et",
+            "eu", "fa", "fi", "fil", "fr", "fy", "ga", "gd", "gl", "gu",
+            "ha", "haw", "he", "hi", "hmn", "hr", "ht", "hu", "hy", "id",
+            "ig", "is", "it", "ja", "jv", "ka", "kk", "km", "kn", "ko",
+            "ku", "ky", "la", "lb", "lo", "lt", "lv", "mg", "mi", "mk",
+            "ml", "mn", "mr", "ms", "mt", "my", "ne", "nl", "no", "ny",
+            "pa", "pl", "ps", "pt", "ro", "ru", "sd", "si", "sk", "sl",
+            "sm", "sn", "so", "sq", "sr", "st", "su", "sv", "sw", "ta",
+            "te", "tg", "th", "tr", "uk", "ur", "uz", "vi", "xh", "yi",
+            "yo", "zh", "zu");
 
     private final int currentType;
 
