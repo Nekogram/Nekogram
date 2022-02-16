f83ae0da1d643dcabd9b9737289702cd6244467d
tehcneko <7741672-NekoInverter@users.noreply.gitlab.com>
2022-02-16 18:43:14 +0800

    Replace condensed font with system font

--- TMessagesProj/src/main/java/org/telegram/messenger/AndroidUtilities.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/AndroidUtilities.java
@@ -1352,20 +1352,9 @@ public class AndroidUtilities {
                         case "fonts/mw_bold.ttf":
                             t = Typeface.create("serif", Typeface.BOLD);
                             break;
-                    }
-                    if (t == null) {
-                        if (Build.VERSION.SDK_INT >= 26) {
-                            Typeface.Builder builder = new Typeface.Builder(ApplicationLoader.applicationContext.getAssets(), assetPath);
-                            if (assetPath.contains("medium")) {
-                                builder.setWeight(700);
-                            }
-                            if (assetPath.contains("italic")) {
-                                builder.setItalic(true);
-                            }
-                            t = builder.build();
-                        } else {
-                            t = Typeface.createFromAsset(ApplicationLoader.applicationContext.getAssets(), assetPath);
-                        }
+                        case "fonts/rcondensedbold.ttf":
+                            t = Typeface.create("sans-serif-condensed", Typeface.BOLD);
+                            break;
                     }
                     typefaceCache.put(assetPath, t);
                 } catch (Exception e) {
