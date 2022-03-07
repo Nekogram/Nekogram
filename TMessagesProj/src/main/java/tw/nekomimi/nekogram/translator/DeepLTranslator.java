733f26a2713c45f5275da60c702681c3615fd06f
NekoInverter <chsqwyx@gmail.com>
2022-03-07 16:24:38 +0800

    Update DeepL

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/translator/DeepLTranslator.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/translator/DeepLTranslator.java
@@ -2,18 +2,12 @@ package tw.nekomimi.nekogram.translator;
 
 import android.text.TextUtils;
 
-import org.deepl.DeepLTranslater;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.telegram.messenger.FileLog;
+import org.deepl.DeepLTranslaterAndroid;
 
-import java.io.IOException;
-import java.net.URLEncoder;
 import java.util.Arrays;
 import java.util.List;
 
 import tw.nekomimi.nekogram.NekoConfig;
-import tw.nekomimi.nekogram.helpers.remote.ConfigHelper;
 
 public class DeepLTranslator extends BaseTranslator {
 
@@ -27,10 +21,7 @@ public class DeepLTranslator extends BaseTranslator {
             "es", "et", "fi", "fr", "hu", "it", "ja", "lt", "lv", "nl",
             "pl", "pt-BR", "pt-PT", "pt", "ro", "ru", "sk", "sl", "sv",
             "zh");
-    private final DeepLTranslater deeplTranslater = new DeepLTranslater();
-
-    private boolean triedApi = false;
-    private String lastKey = NekoConfig.lastDeepLKey;
+    private final DeepLTranslaterAndroid deeplTranslater = new DeepLTranslaterAndroid();
 
     static DeepLTranslator getInstance() {
         if (instance == null) {
@@ -67,83 +58,11 @@ public class DeepLTranslator extends BaseTranslator {
 
     @Override
     protected Result translate(String query, String tl) throws Exception {
-        Result result = null;
-        if (!triedApi) {
-            if (!TextUtils.isEmpty(lastKey)) {
-                try {
-                    result = translateApiImpl(lastKey, query, tl);
-                } catch (Exception e) {
-                    FileLog.e(e);
-                }
-            }
-            if (result == null) {
-                for (String key : ConfigHelper.getDeepLKeys()) {
-                    try {
-                        result = translateApiImpl(key, query, tl);
-                        if (result != null) {
-                            lastKey = key;
-                            NekoConfig.setLastDeepLKey(lastKey);
-                            break;
-                        }
-                    } catch (Exception e) {
-                        FileLog.e(e);
-                    }
-                }
-            }
-
-        }
-        if (result == null) {
-            if (!triedApi) {
-                triedApi = true;
-                ConfigHelper.getInstance().load();
-            }
-            result = translateWebImpl(query, tl);
-        }
-        return result;
-    }
-
-    private Result translateApiImpl(String key, String query, String tl) throws IOException, JSONException {
-        var params = "text=" + URLEncoder.encode(query, "UTF-8") +
-                "&target_lang=" + tl.toUpperCase() +
-                "&formality=" + getFormalityStringApi();
-        String response = Http.url("https://api" + (key.endsWith(":fx") ? "-free" : "") + ".deepl.com/v2/translate")
-                .header("Authorization", "DeepL-Auth-Key " + key)
-                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36")
-                .data(params)
-                .request();
-        if (TextUtils.isEmpty(response)) {
-            return null;
-        }
-        var jsonObject = new JSONObject(response);
-        if (!jsonObject.has("translations") && jsonObject.has("message")) {
-            throw new IOException(jsonObject.getString("message"));
-        }
-        var translation = jsonObject.getJSONArray("translations").getJSONObject(0);
-        String sourceLang = null;
-        if (translation.has("detected_source_language")) {
-            sourceLang = translation.getString("detected_source_language");
-        }
-        return new Result(translation.getString("text"), sourceLang);
-    }
-
-    private Result translateWebImpl(String query, String tl) throws Exception {
-        var result = deeplTranslater.translate(query, "auto", tl.toUpperCase(), getFormalityStringWeb());
+        var result = deeplTranslater.translate(query, tl, getFormalityString(), "newlines");
         return new Result(result[1], result[0]);
     }
 
-    private String getFormalityStringApi() {
-        switch (NekoConfig.deepLFormality) {
-            case FORMALITY_DEFAULT:
-            default:
-                return "default";
-            case FORMALITY_MORE:
-                return "more";
-            case FORMALITY_LESS:
-                return "less";
-        }
-    }
-
-    private String getFormalityStringWeb() {
+    private String getFormalityString() {
         switch (NekoConfig.deepLFormality) {
             case FORMALITY_DEFAULT:
             default:
