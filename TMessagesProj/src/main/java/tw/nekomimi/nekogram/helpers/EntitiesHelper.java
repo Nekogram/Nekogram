088b0817b49f030f69c51b79efdd3136fc012905
tehcneko <7741672-NekoInverter@users.noreply.gitlab.com>
2022-07-22 10:29:20 +0800

    Use TG spans for new markdown parser

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/EntitiesHelper.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/EntitiesHelper.java
@@ -1,22 +1,17 @@
 package tw.nekomimi.nekogram.helpers;
 
-import android.graphics.Typeface;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextUtils;
-import android.text.style.BackgroundColorSpan;
-import android.text.style.LocaleSpan;
-import android.text.style.StrikethroughSpan;
-import android.text.style.StyleSpan;
-import android.text.style.TypefaceSpan;
-import android.text.style.URLSpan;
 
 import org.telegram.messenger.LinkifyPort;
-import org.telegram.ui.ActionBar.Theme;
+import org.telegram.messenger.MediaDataController;
+import org.telegram.tgnet.TLRPC;
+import org.telegram.ui.Components.TextStyleSpan;
+import org.telegram.ui.Components.URLSpanReplacement;
 
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.regex.Pattern;
 
 import tw.nekomimi.nekogram.NekoConfig;
@@ -39,7 +34,6 @@ public class EntitiesHelper {
         return message[0];
     }
 
-    // TODO: refactor to use tg spans
     public static void parseMarkdown(CharSequence[] message, boolean allowStrike) {
         var spannable = message[0] instanceof Spannable ? (Spannable) message[0] : Spannable.Factory.getInstance().newSpannable(message[0]);
         for (int i = 0; i < PATTERNS.length; i++) {
@@ -54,44 +48,50 @@ public class EntitiesHelper {
                 var start = m.start();
                 var end = m.end();
                 var length = i < 3 ? 3 : i > 3 && i != 8 ? 2 : 1;
-                var typefaceSpans = spannable.getSpans(start, end, TypefaceSpan.class);
-                for (var typefaceSpan : typefaceSpans) {
-                    if (!"monospace".equals(typefaceSpan.getFamily())) {
+                var textStyleSpans = spannable.getSpans(start, end, TextStyleSpan.class);
+                for (var textStyleSpan : textStyleSpans) {
+                    if (!textStyleSpan.isMono()) {
                         continue;
                     }
-                    int spanStart = spannable.getSpanStart(typefaceSpan);
-                    int spanEnd = spannable.getSpanEnd(typefaceSpan);
+                    int spanStart = spannable.getSpanStart(textStyleSpan);
+                    int spanEnd = spannable.getSpanEnd(textStyleSpan);
                     if (spanStart < start + length || spanEnd > end - length) {
                         continue find;
                     }
                 }
 
                 var destination = new SpannableStringBuilder(spannable.subSequence(m.start(i == 0 ? 2 : 1), m.end(i == 0 ? 2 : 1)));
-                switch (i) {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                        if (i != 3) {
-                            destination.setSpan(new LocaleSpan(languageToLocale(i == 0 ? m.group(1) : "")), 0, destination.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        }
-                        destination.setSpan(new TypefaceSpan("monospace"), 0, destination.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        break;
-                    case 4:
-                        destination.setSpan(new StyleSpan(Typeface.BOLD), 0, destination.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        break;
-                    case 5:
-                        destination.setSpan(new StyleSpan(Typeface.ITALIC), 0, destination.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        break;
-                    case 6:
-                        destination.setSpan(new StrikethroughSpan(), 0, destination.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        break;
-                    case 7:
-                        destination.setSpan(new BackgroundColorSpan(Theme.getColor(Theme.key_chats_archivePullDownBackground)), 0, destination.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        break;
-                    case 8:
-                        destination.setSpan(new URLSpan(m.group(2)), 0, destination.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        break;
+                if (i < 8) {
+                    var run = new TextStyleSpan.TextStyleRun();
+                    switch (i) {
+                        case 0:
+                        case 1:
+                        case 2:
+                        case 3:
+                            run.flags |= TextStyleSpan.FLAG_STYLE_MONO;
+                            if (i != 3) {
+                                run.start = start;
+                                run.end = end;
+                                run.urlEntity = new TLRPC.TL_messageEntityPre();
+                                run.urlEntity.language = i == 0 ? m.group(1) : "";
+                            }
+                            break;
+                        case 4:
+                            run.flags |= TextStyleSpan.FLAG_STYLE_BOLD;
+                            break;
+                        case 5:
+                            run.flags |= TextStyleSpan.FLAG_STYLE_ITALIC;
+                            break;
+                        case 6:
+                            run.flags |= TextStyleSpan.FLAG_STYLE_STRIKE;
+                            break;
+                        case 7:
+                            run.flags |= TextStyleSpan.FLAG_STYLE_SPOILER;
+                            break;
+                    }
+                    MediaDataController.addStyleToText(new TextStyleSpan(run), 0, destination.length(), destination, true);
+                } else {
+                    destination.setSpan(new URLSpanReplacement(m.group(2)), 0, destination.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                 }
                 sources.add(m.group(0));
                 destinations.add(destination);
@@ -101,9 +101,5 @@ public class EntitiesHelper {
             }
         }
         message[0] = spannable;
-    }
-
-    private static Locale languageToLocale(String language) {
-        return Locale.forLanguageTag("ng-SH-x-lvariant-lang-" + (TextUtils.isEmpty(language) ? "none" : language));
     }
 }
