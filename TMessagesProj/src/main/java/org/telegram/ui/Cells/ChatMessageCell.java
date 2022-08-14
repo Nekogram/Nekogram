2d4786b11f53cc57b3ca42b920491683b0ba0e24
tehcneko <chsqwyx@gmail.com>
2022-08-14 17:00:10 +0800

    Fix text position in bubbles
    official bug

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
@@ -8525,7 +8525,7 @@ public class ChatMessageCell extends BaseCell implements SeekBar.SeekBarDelegate
                     textX -= AndroidUtilities.dp(4);
                 }
             } else {
-                textY = AndroidUtilities.dp(8) + namesOffset;
+                textY = AndroidUtilities.dp(10) + namesOffset;
             }
             unmovedTextX = textX;
             if (currentMessageObject.textXOffset != 0 && replyNameLayout != null) {
