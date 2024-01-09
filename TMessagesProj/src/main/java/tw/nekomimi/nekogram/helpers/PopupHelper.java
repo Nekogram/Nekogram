468d257fe4494d298e7416eeb4e52ac78912efa1
tehcneko <chsqwyx@gmail.com>
2024-01-09 14:40:03 +0800

    Remove simple menu

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/PopupHelper.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/PopupHelper.java
@@ -7,9 +7,9 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import org.telegram.messenger.AndroidUtilities;
-import org.telegram.messenger.FileLog;
 import org.telegram.messenger.LocaleController;
 import org.telegram.messenger.R;
+import org.telegram.messenger.Utilities;
 import org.telegram.ui.ActionBar.ActionBarMenuItem;
 import org.telegram.ui.ActionBar.ActionBarMenuSubItem;
 import org.telegram.ui.ActionBar.ActionBarPopupWindow;
@@ -23,53 +23,31 @@ import org.telegram.ui.Components.BulletinFactory;
 import java.util.ArrayList;
 
 import tw.nekomimi.nekogram.DatacenterPopupWrapper;
-import tw.nekomimi.nekogram.simplemenu.SimpleMenuPopupWindow;
 
 public class PopupHelper {
-    private static SimpleMenuPopupWindow mPopupWindow;
 
-    public static void show(ArrayList<? extends CharSequence> entries, String title, int checkedIndex, Context context, View itemView, SimpleMenuPopupWindow.OnItemClickListener listener, Theme.ResourcesProvider resourcesProvider) {
-        if (true) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(context, resourcesProvider);
-            builder.setTitle(title);
-            final LinearLayout linearLayout = new LinearLayout(context);
-            linearLayout.setOrientation(LinearLayout.VERTICAL);
-            builder.setView(linearLayout);
+    public static void show(ArrayList<? extends CharSequence> entries, String title, int checkedIndex, Context context, View itemView, Utilities.Callback<Integer> listener, Theme.ResourcesProvider resourcesProvider) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, resourcesProvider);
+        builder.setTitle(title);
+        final LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        builder.setView(linearLayout);
 
-            for (int a = 0; a < entries.size(); a++) {
-                RadioColorCell cell = new RadioColorCell(context, resourcesProvider);
-                cell.setPadding(AndroidUtilities.dp(4), 0, AndroidUtilities.dp(4), 0);
-                cell.setTag(a);
-                cell.setTextAndValue(entries.get(a), checkedIndex == a);
-                cell.setBackground(Theme.createSelectorDrawable(Theme.getColor(Theme.key_listSelector, resourcesProvider), Theme.RIPPLE_MASK_ALL));
-                linearLayout.addView(cell);
-                cell.setOnClickListener(v -> {
-                    Integer which = (Integer) v.getTag();
-                    builder.getDismissRunnable().run();
-                    listener.onClick(which);
-                });
-            }
-            builder.setNegativeButton(LocaleController.getString(R.string.Cancel), null);
-            builder.show();
-        } else {
-            View container = (View) itemView.getParent();
-            if (container == null) {
-                return;
-            }
-            if (mPopupWindow != null) {
-                try {
-                    if (mPopupWindow.isShowing()) mPopupWindow.dismiss();
-                } catch (Exception e) {
-                    FileLog.e(e);
-                }
-            }
-            mPopupWindow = new SimpleMenuPopupWindow(context, resourcesProvider);
-            mPopupWindow.setOnItemClickListener(listener);
-            mPopupWindow.setEntries(entries.toArray(new CharSequence[0]));
-            mPopupWindow.setSelectedIndex(checkedIndex);
-
-            mPopupWindow.show(itemView, container, 0);
+        for (int a = 0; a < entries.size(); a++) {
+            RadioColorCell cell = new RadioColorCell(context, resourcesProvider);
+            cell.setPadding(AndroidUtilities.dp(4), 0, AndroidUtilities.dp(4), 0);
+            cell.setTag(a);
+            cell.setTextAndValue(entries.get(a), checkedIndex == a);
+            cell.setBackground(Theme.createSelectorDrawable(Theme.getColor(Theme.key_listSelector, resourcesProvider), Theme.RIPPLE_MASK_ALL));
+            linearLayout.addView(cell);
+            cell.setOnClickListener(v -> {
+                Integer which = (Integer) v.getTag();
+                builder.getDismissRunnable().run();
+                listener.run(which);
+            });
         }
+        builder.setNegativeButton(LocaleController.getString(R.string.Cancel), null);
+        builder.show();
     }
 
     public static void showIdPopup(BaseFragment fragment, View anchorView, long id, int dc, boolean user, float x, float y) {
