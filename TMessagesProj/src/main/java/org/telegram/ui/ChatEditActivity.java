cff55b650b028fa7320454660c44c94148d6aecb
tehcneko <chsqwyx@gmail.com>
2024-12-26 16:48:07 +0800

    Fix affiliate showing up without permission

--- TMessagesProj/src/main/java/org/telegram/ui/ChatEditActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatEditActivity.java
@@ -1181,7 +1181,7 @@ public class ChatEditActivity extends BaseFragment implements ImageUpdater.Image
                 infoContainer.addView(channelAffiliateProgramsCell, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
             }
 
-            if (channelAffiliateProgramsCell != null && getMessagesController().starrefConnectAllowed) {
+            if (channelAffiliateProgramsCell != null && ChatObject.hasAdminRights(currentChat) && getMessagesController().starrefConnectAllowed) {
                 channelAffiliateProgramsCell.setVisibility(View.VISIBLE);
             }
             if (logCell != null) {
