4eb4f33cf32f8233f4f9fe6f4b54a5a894b51508
tehcneko <chsqwyx@gmail.com>
2025-06-05 19:09:12 +0800

    Disable over scroll for filter tabs

--- TMessagesProj/src/main/java/org/telegram/ui/Components/FilterTabsView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/FilterTabsView.java
@@ -1025,6 +1025,7 @@ public class FilterTabsView extends FrameLayout {
             }
         };
         listView.setClipChildren(false);
+        listView.setOverScrollMode(OVER_SCROLL_NEVER);
         itemAnimator = new DefaultItemAnimator() {
 
             @Override
