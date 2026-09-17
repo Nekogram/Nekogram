4998904aaf22472e477ee9a9e87cb46f6b0e4006
tehcneko <chsqwyx@gmail.com>
2026-09-17 18:39:02 +0800

    Acquire Wi-Fi lock during calling

--- TMessagesProj/src/main/java/org/telegram/messenger/voip/VoIPService.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/voip/VoIPService.java
@@ -63,6 +63,7 @@ import android.media.audiofx.NoiseSuppressor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -235,6 +236,7 @@ public class VoIPService extends Service implements SensorEventListener, AudioMa
 	private String lastError;
 	private PowerManager.WakeLock proximityWakelock;
 	private PowerManager.WakeLock cpuWakelock;
+	private WifiManager.WifiLock wifiLock;
 	private boolean isProximityNear;
 	private boolean isHeadsetPlugged;
 	private int previousAudioOutput = -1;
@@ -4214,6 +4216,9 @@ public class VoIPService extends Service implements SensorEventListener, AudioMa
 			}
 		}
 		cpuWakelock.release();
+		if (wifiLock != null) {
+			wifiLock.release();
+		}
 		AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
 		if (!playingSound) {
 			VoipAudioManager vam = VoipAudioManager.get();
@@ -4649,6 +4654,13 @@ public class VoIPService extends Service implements SensorEventListener, AudioMa
 
 			cpuWakelock = ((PowerManager) getSystemService(POWER_SERVICE)).newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "telegram-voip");
 			cpuWakelock.acquire();
+			var wm = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+			if (wm != null) {
+				wifiLock = wm.createWifiLock(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+						? WifiManager.WIFI_MODE_FULL_LOW_LATENCY
+						: WifiManager.WIFI_MODE_FULL_HIGH_PERF, "telegram-voip");
+				wifiLock.acquire();
+			}
 
 			btAdapter = am.isBluetoothScoAvailableOffCall() ? BluetoothAdapter.getDefaultAdapter() : null;
 
